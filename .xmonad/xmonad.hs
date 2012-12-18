3874a86be2096409c28d734e1c40117974eac720
PierreR <pradermecker@yahoo.ca>
2012-12-18 12:47:36 +0100

    Xmonad with Mirror tiled first.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -146,7 +146,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = tiled ||| Mirror tiled ||| Full
+myLayout = Mirror tiled ||| tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
