0a31d0443dca232a1022a13f47c286596ceaec14
Pierre Radermecker <pierrer@pi3r.be>
2015-05-03 12:21:58 +0200

    update nix config

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -1,12 +1,15 @@
 {
+  provideOldHaskellAttributeNames = true;
+
   packageOverrides = super: let self = super.pkgs; in
   {
-    hsEnv = self.haskellngPackages.ghcWithPackages (p: with p; [
-      async attoparsec ghc-mod base16-bytestring bytestring case-insensitive containers
+    hsDevEnv = self.haskellngPackages.ghcWithPackages (p: with p; [
+      async attoparsec base16-bytestring bytestring case-insensitive containers
       cryptohash Diff directory either exceptions filecache Glob
-      hashable hslogger hspec hlint HUnit lens lens-aeson
-      mtl operational optparse-applicative parallel-io
-      scientific strict-base-types vector yaml
+      hashable hslogger hspec hlint HUnit lens lens-aeson pcre-utils
+      mtl operational optparse-applicative parallel-io regex-pcre-builtin
+      scientific servant servant-client strict-base-types vector yaml
     ]);
+
   };
 }
